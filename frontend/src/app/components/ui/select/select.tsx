cf0ed082f474dc3272f194cebe5f5ead3953a144
Svyatoslav Semenov <semenovslava293@gmail.com>
2024-04-07 00:24:03 +0300

    добавил компонент select

---
+++ frontend/src/app/components/ui/select/select.tsx
@@ -0,0 +1,23 @@
+import React, {FC} from 'react';
+import classes from "./select.module.scss";
+
+type Props = {
+    name: string,
+    options: string[],
+}
+
+const Select: FC<Props> = ({options, name}) => {
+    if (options.length === 0) {
+        return <div>Добавь опций!!!!</div>
+    }
+
+    return (
+        <select name={name} className={classes.select}>
+            {options.map(option => (
+                <option key={option} className={classes.select__option} value={option}>{option}</option>
+            ))}
+        </select>
+    );
+};
+
+export default Select;
