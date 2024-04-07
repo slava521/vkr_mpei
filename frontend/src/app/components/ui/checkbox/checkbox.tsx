d2683afca82b0343ed34ced97feab5d3e567c660
Svyatoslav Semenov <semenovslava293@gmail.com>
2024-04-07 12:06:30 +0300

    добавил компонент чекбокса

---
+++ frontend/src/app/components/ui/checkbox/checkbox.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import {FC, useId} from "react";
+import classes from "./checkbox.module.scss";
+
+type Props = {
+    label: string,
+    name: string,
+}
+
+const Checkbox:FC<Props> = ({label, name}) => {
+    const id = useId()
+
+    return (
+        <div className={classes.checkbox}>
+            <input id={id} type="checkbox" name={name} className={classes.checkbox__input}/>
+            <label htmlFor={id} className={classes.checkbox__label}>{label}</label>
+        </div>
+    );
+};
+
+export default Checkbox;
