0373569405056cf2d67dc83d7e0d60b304ec97fc
Svyatoslav Semenov <semenovslava293@gmail.com>
2024-04-07 12:59:18 +0300

    поправил компонент радио кнопки

---
+++ frontend/src/app/components/ui/radioButton/radioButton.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React, {FC, useId} from 'react';
+import classes from "./radioButton.module.scss";
+
+type Props = {
+    value: string,
+    name: string,
+    label: string,
+    checked?: boolean
+}
+
+const RadioButton:FC<Props> = ({value, name, label, checked}) => {
+    const id = useId()
+
+    return (
+        <div>
+            <input id={id} type="radio" className={classes.radioButton} name={name} value={value} checked={!!checked}/>
+            <label htmlFor={id} className={classes.radioButton__label}>{label}</label>
+        </div>
+    );
+};
+
+export default RadioButton;
