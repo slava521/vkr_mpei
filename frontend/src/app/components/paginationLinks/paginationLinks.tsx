470be9f57638c0691f19f6acf4c167e004df3026
Svyatoslav Semenov <semenovslava293@gmail.com>
2024-04-14 15:18:37 +0300

    поправил компонент для пагинации

--- frontend/src/app/components/paginationLinks/paginationLinks.tsx
+++ frontend/src/app/components/paginationLinks/paginationLinks.tsx
@@ -22,33 +22,41 @@ const PaginationLinks: FC<Props> = ({contentCount}) => {
 
     return (
         <div className={classes.paginationLinks}>
-            <Link href={`?page=${1}`} className={classes.paginationLinks__link}>1</Link>
+            {currentPage !== 1 &&
+                <Link href={`?page=${1}`} className={classes.paginationLinks__link}>1</Link>
+            }
             {currentPage === 4 &&
-                <Link href={`?page=${2}`}
-                      className={classes.paginationLinks__link}>2</Link>
+                <Link href={`?page=${2}`} className={classes.paginationLinks__link}>2</Link>
             }
             {currentPage > 4 &&
                 <div className={classes.paginationLinks__link}>...</div>
             }
             {currentPage !== 1 && currentPage !== 2 &&
-                <Link href={`?page=${currentPage - 1}`}
-                      className={classes.paginationLinks__link}>{currentPage - 1}</Link>
+                <Link href={`?page=${currentPage - 1}`} className={classes.paginationLinks__link}>
+                    {currentPage - 1}
+                </Link>
             }
-            {currentPage !== 1 && currentPage !== pagesCount &&
-                <div className={classes.paginationLinks__link}>{currentPage}</div>
-            }
+            <div className={`${classes.paginationLinks__link} ${classes['paginationLinks__link--current']}`}>
+                {currentPage}
+            </div>
             {currentPage !== pagesCount && currentPage !== pagesCount - 1 &&
-                <Link href={`?page=${currentPage + 1}`}
-                      className={classes.paginationLinks__link}>{currentPage + 1}</Link>
+                <Link href={`?page=${currentPage + 1}`} className={classes.paginationLinks__link}>
+                    {currentPage + 1}
+                </Link>
             }
             {currentPage < pagesCount - 3 &&
                 <div className={classes.paginationLinks__link}>...</div>
             }
             {currentPage === pagesCount - 3 &&
-                <Link href={`?page=${pagesCount - 3}`}
-                      className={classes.paginationLinks__link}>{pagesCount - 3}</Link>
+                <Link href={`?page=${pagesCount - 3}`} className={classes.paginationLinks__link}>
+                    {pagesCount - 3}
+                </Link>
             }
-            <Link href={`?page=${pagesCount}`} className={classes.paginationLinks__link}>{pagesCount}</Link>
+            {currentPage !== pagesCount &&
+                <Link href={`?page=${pagesCount}`} className={classes.paginationLinks__link}>
+                    {pagesCount}
+                </Link>
+            }
         </div>
     );
 };
